6de0a5a2c3d2ee875d26a1ae5b5ad6a80bf50e1f
zperkins321 <zperkins@college.harvard.edu>
2014-04-18 20:13:56 -0400

    ab language

---
+++ ab/raw/gen.js
@@ -0,0 +1,9 @@
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '../../../../skins';
+
+/* MediaWiki:Common.js */
+/* Размещённый здесь код JavaScript будет загружен всем пользователям при обращении к какой-либо странице */
+
+/* MediaWiki:Monobook.js */
+/* Устарело. Используйте [[MediaWiki:common.js]] */
