441e6dbd35047655eed7df5bb8368d29d824908e
zperkins321 <zperkins@college.harvard.edu>
2014-04-18 20:38:35 -0400

    deleted ab

--- ab/raw/gen.js
+++
@@ -1,9 +0,0 @@
-/* generated javascript */
-var skin = 'monobook';
-var stylepath = '../../../../skins';
-
-/* MediaWiki:Common.js */
-/* Размещённый здесь код JavaScript будет загружен всем пользователям при обращении к какой-либо странице */
-
-/* MediaWiki:Monobook.js */
-/* Устарело. Используйте [[MediaWiki:common.js]] */
--- ab/skins/common/blank.php
+++
@@ -1,1 +0,0 @@
-<?php header("Cache-Control: s-maxage=2678400, max-age=2678400");
