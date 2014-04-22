61c7a85d0efca19236bf1d3502f703bd229f0ef1
zperkins321 <zperkins@college.harvard.edu>
2014-04-22 17:31:25 -0400

    shit

--- src/analytics.py
+++ src/analytics.py
@@ -10,15 +10,12 @@ global marked
 
 
 def load_dict(path):
-	f = open(path)
-	text = f.read
-	pat = re.compile('<a href=[\'"]?([^\'" >]+)')
-    links = re.findall(pat, text)
+	links_to_follow = links = extract_links(path)
     while len(links) > 0:
     	if links[0] in dict.keys():
     		popular[links[0]] += 1; #update frequency of link
     	else:
     		popular[links.pop(0)] = 1; #add element to dictionary
-    for x in links:
+    for x in links_to_follow:
     	load_dict(x)
 
