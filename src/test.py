8fbe96364d4f42ee378dec8010a0b944106bffdb
zperkins321 <zperkins@college.harvard.edu>
2014-04-25 00:34:11 -0400

    clean

--- src/test.py
+++ src/test.py
@@ -3,13 +3,6 @@
 import analytics
 import bfs
 
-my_dict = analytics.load_links()
-'''
-for x in my_dict.keys():
-	print x
-'''
-
-
-print analytics.most_links()
-print analytics.center()
+print "The most popular page is... %s" % analytics.most_links()
+print "The center of wikipedia is... %s" % analytics.center()
 
