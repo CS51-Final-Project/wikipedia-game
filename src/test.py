cad9e1f3c59a1a2b50d9b78cef76a59281bd4696
zperkins321 <zperkins@college.harvard.edu>
2014-04-24 23:37:18 -0400

    dict

--- src/test.py
+++ src/test.py
@@ -4,5 +4,8 @@ import bfs
 import analytics
 
 # print analytics.most_links()
-print bfs.center()
+# print bfs.center()
+for keys,values in analytics.all_links():
+    print(keys)
+    print(values)
 
