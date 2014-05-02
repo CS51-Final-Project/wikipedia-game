f2d3b712517d7c1fe4f7a931ae1439e700ea41a6
zperkins321 <zperkins@college.harvard.edu>
2014-05-02 14:46:08 -0400

    finished commenting analytics and find

--- code/analytics.py
+++ code/analytics.py
@@ -5,28 +5,33 @@ import bfs
 import sets
 import os
 
-#dictionary for links
+#dictionary for popular links
 popular = {}
+#set that keeps track of visited links
 visited = sets.Set()
 
+#generate the correct path for any user
 WIKI_DIR = os.path.abspath("../data") + "/"
+
+#dictionary for holding all links for compressed dictionary
 all_links = {}
 
+#creates a compressed dictionary of solely articles with their links
 def load_links():
-    for x in os.listdir(WIKI_DIR):
-        all_links[x.lower()] = bfs.extract_links(WIKI_DIR + x)
+    for x in os.listdir(WIKI_DIR): #for each article
+        all_links[x.lower()] = bfs.extract_links(WIKI_DIR + x) #extract its links
     return all_links
 
 #only gets all links from one path
 def load_dict(dictionary):
     for key in dictionary.keys(): #loop for all keys in dictionary
-        for link in dictionary[key]: #keep track of links we've visited
-            visited.add(link)
+        for link in dictionary[key]: 
+            visited.add(link) #keep track of links we've visited
         for links in dictionary[key]:
             if links in popular.keys():
-                popular[links] += 1; #update frequency of link
+                popular[links] += 1; #update frequency of link if its already in dict
             else:
-                popular[links] = 1; #add element to dictionary
+                popular[links] = 1; #add element to dictionary if its not
     return popular
 
 #returns key from dictionary with the most links
@@ -34,28 +39,29 @@ def most_links(dictionary):
     load_dict(dictionary) #create a popular dictionary from the condensed dictionary
     return (sorted(popular, key=popular.__getitem__, reverse=True))[:3] #return the most popular
 
-
+#finds the center of the wikipedia directory (article that links to the most pages)
 def center(dictionary):
-    center_dict = {}
-    pages = os.listdir(WIKI_DIR)
+    center_dict = {} #initializes a center dictionary
+    pages = os.listdir(WIKI_DIR) #grabs all articles
     counter = 1
     length = len(pages)
     for p in pages:
-        print "{0}/{1} - {2} BFSed".format(counter, length, p)
+        print "{0}/{1} - {2} BFSed".format(counter, length, p) #prints to command line for progress check
         counter += 1
-        center_dict[p] = len(bfs.BFS(p, wiki_dict = dictionary).keys())
-    return sorted(center_dict, key = center_dict.__getitem__, reverse = True)[0]
+        center_dict[p] = len(bfs.BFS(p, wiki_dict = dictionary).keys()) #runs a BFS on each page and fills center dict with that page and the number of pages it reaches
+    return sorted(center_dict, key = center_dict.__getitem__, reverse = True)[0] #returns the page with highest value
 
+#returns all pages that reference a certain page
 def where_referenced(link, dictionary):
-    load_dict(dictionary)
-    references = sets.Set()
-    for key in popular.keys():
-        if link in key:
-            references.add(key)
+    load_dict(dictionary) #loads popular dictionary
+    references = sets.Set() #initializes a new set for references
+    for key in popular.keys(): #for each page
+        if link in key: #if it references the page of interest
+            references.add(key) #add it to the list of references
     try:
         print "{0} is referenced on the following pages:".format(link)
         for page in references:
-            print page
+            print page #prints all references cleanly
         print "\n"
     except(Exception):
         print "Article not in dictionary"
--- code/find.py
+++ code/find.py
@@ -3,23 +3,36 @@ import sys
 import analytics
 import bfs
 
+#loads a compressed dictionary to be used in all functions
 wiki_dict = analytics.load_links()
+
+#runs BFS on the two articles
 shortest_path = bfs.BFS((sys.argv[1]).lower(), (sys.argv[2]).lower(), wiki_dict)
+
+#if the destination article is a few pages away, returns the path
+#else says article is on page
 if len(shortest_path) > 2:
 	print "The shortest path between {0} and {1} is following the following links: \n {2}".format((sys.argv[1]).lower(), (sys.argv[2]).lower(), shortest_path)
 else:
 	print "{1} is a direct link from {0}".format(sys.argv[2], sys.argv[1])
 
-
+#if user wants analytics and provides "-analytics" then runs
 if len(sys.argv) == 4 and sys.argv[3] == "-analytics":
 	print "\n"
+	#checks references for the starting article
 	raw_input("List pages that reference {0}? ".format(sys.argv[1]))
 	analytics.where_referenced((sys.argv[1]).lower(), wiki_dict)
+
+	#checks references for the destination article
 	raw_input("List pages that reference {0}? ".format(sys.argv[2]))
 	analytics.where_referenced((sys.argv[2]).lower(), wiki_dict)
+
+	#finds the most popular page on wikipedia
 	raw_input("Find the most popular page on Wikipedia? ")
 	print "The most popular three articles in Wikipedia are: \n{0}".format(analytics.most_links(wiki_dict))
 	print "\n"
+
+	#finds the center of wikipedia
 	raw_input("Continue? ")
 	print "Initializing Microprocessors..."
 	raw_input("Start Search for Center? ")
