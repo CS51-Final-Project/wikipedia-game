6384c258db8f95ac72f66174e99d60dcd7c80fa4
zperkins321 <zperkins@college.harvard.edu>
2014-05-01 23:24:14 -0400

    center

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import bfs
 wiki_dict = analytics.load_links()
 
 print "The most popular article in Wikipedia is {0}".format(analytics.most_links(wiki_dict))
-#print "The center of Wikipedia is {0}".format(analytics.center(wiki_dict))
+print "The center of Wikipedia is {0}".format(analytics.center(wiki_dict))
 
 print "The shortest path between {0} and {1} is following the following links: \n {2}".format(sys.argv[1], sys.argv[2], (bfs.BFS(sys.argv[1], sys.argv[2], wiki_dict)))
 analytics.link_frequency(sys.argv[1], wiki_dict)
