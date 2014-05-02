a1b2c0daa3752c4f469578f0b49ed8b80df346e1
zperkins321 <zperkins@college.harvard.edu>
2014-05-02 17:29:42 -0400

    asdfsd

--- code/find.py
+++ code/find.py
@@ -11,10 +11,12 @@ shortest_path = bfs.BFS((sys.argv[1]).lower(), (sys.argv[2]).lower(), wiki_dict)
 
 #if the destination article is a few pages away, returns the path
 #else says article is on page
-if len(shortest_path) > 2:
+if type(shortest_path) == dict:
+	print "The two articles are in no way connected"
+elif len(shortest_path) > 2:
 	print "The shortest path between {0} and {1} is following the following links: \n {2}".format((sys.argv[1]).lower(), (sys.argv[2]).lower(), shortest_path)
 else:
-	print "{1} is a direct link from {0}".format(sys.argv[2], sys.argv[1])
+	print "{0} is a direct link from {1}".format(sys.argv[2], sys.argv[1])
 
 #if user wants analytics and provides "-analytics" then runs
 if len(sys.argv) == 4 and sys.argv[3] == "-analytics":
