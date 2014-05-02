915e8c76ac87b4f1a64935c9e37f885443abc786
zperkins321 <zperkins@college.harvard.edu>
2014-05-01 23:10:42 -0400

    braces

--- src/test.py
+++ src/test.py
@@ -5,9 +5,9 @@ import bfs
 
 wiki_dict = analytics.load_links()
 
-print "The most popular article in Wikipedia is %s" % analytics.most_links(wiki_dict)
+print "The most popular article in Wikipedia is {0}".format(analytics.most_links(wiki_dict))
 print "The center of Wikipedia is %s" analytics.center(wiki_dict)
 
-print "The shortest path between %s and %s is following the following links: \n %s" % argv[1], argv[2], (bfs.BFS(argv[1], argv[2], wiki_dict))
+print "The shortest path between {0} and {1} is following the following links: \n {2}".format(argv[1], argv[2], (bfs.BFS(argv[1], argv[2], wiki_dict)))
 analytics.link_frequency(argv[1], wiki_dict)
 analytics.where_referenced(argv[1], wiki_dict)
